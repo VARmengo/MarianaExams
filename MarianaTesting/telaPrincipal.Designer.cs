a35990c56e8b6d13542bb93e04800482739c05f6
caiomoreiradc <caiomoreiracarvalhoo@outlook.com>
2023-07-03 17:01:55 -0300

    Tela principal editada.

--- MarianaTesting/telaPrincipal.Designer.cs
+++ MarianaTesting/telaPrincipal.Designer.cs
@@ -47,6 +47,7 @@
             // 
             // panel1
             // 
+            panel1.BorderStyle = BorderStyle.FixedSingle;
             panel1.Controls.Add(btnMaterias);
             panel1.Controls.Add(btnDisciplinas);
             panel1.Controls.Add(btnQuestions);
@@ -59,64 +60,91 @@
             panel1.Dock = DockStyle.Left;
             panel1.Location = new Point(0, 0);
             panel1.Name = "panel1";
-            panel1.Size = new Size(228, 661);
+            panel1.Size = new Size(231, 661);
             panel1.TabIndex = 0;
             // 
             // btnMaterias
             // 
-            btnMaterias.Font = new Font("Microsoft Sans Serif", 9.75F, FontStyle.Regular, GraphicsUnit.Point);
-            btnMaterias.Location = new Point(1, 375);
+            btnMaterias.Cursor = Cursors.Hand;
+            btnMaterias.FlatStyle = FlatStyle.Flat;
+            btnMaterias.Font = new Font("Microsoft Sans Serif", 9.75F, FontStyle.Bold, GraphicsUnit.Point);
+            btnMaterias.Image = WinApp.Properties.Resources.school;
+            btnMaterias.Location = new Point(3, 375);
             btnMaterias.Name = "btnMaterias";
-            btnMaterias.Size = new Size(227, 40);
+            btnMaterias.Size = new Size(222, 40);
             btnMaterias.TabIndex = 22;
             btnMaterias.Text = "Matérias";
+            btnMaterias.TextAlign = ContentAlignment.MiddleRight;
+            btnMaterias.TextImageRelation = TextImageRelation.ImageBeforeText;
             btnMaterias.UseVisualStyleBackColor = true;
             btnMaterias.Click += btnMaterias_Click;
             // 
             // btnDisciplinas
             // 
-            btnDisciplinas.Font = new Font("Microsoft Sans Serif", 9.75F, FontStyle.Regular, GraphicsUnit.Point);
-            btnDisciplinas.Location = new Point(1, 421);
+            btnDisciplinas.Cursor = Cursors.Hand;
+            btnDisciplinas.FlatStyle = FlatStyle.Flat;
+            btnDisciplinas.Font = new Font("Microsoft Sans Serif", 9.75F, FontStyle.Bold, GraphicsUnit.Point);
+            btnDisciplinas.Image = WinApp.Properties.Resources.subjects;
+            btnDisciplinas.Location = new Point(3, 421);
             btnDisciplinas.Name = "btnDisciplinas";
-            btnDisciplinas.Size = new Size(227, 40);
+            btnDisciplinas.Size = new Size(222, 40);
             btnDisciplinas.TabIndex = 21;
             btnDisciplinas.Text = "Disciplinas";
+            btnDisciplinas.TextAlign = ContentAlignment.MiddleRight;
+            btnDisciplinas.TextImageRelation = TextImageRelation.ImageBeforeText;
             btnDisciplinas.UseVisualStyleBackColor = true;
             btnDisciplinas.Click += btnDisciplinas_Click;
             // 
             // btnQuestions
             // 
-            btnQuestions.Font = new Font("Microsoft Sans Serif", 9.75F, FontStyle.Regular, GraphicsUnit.Point);
-            btnQuestions.Location = new Point(1, 329);
+            btnQuestions.Cursor = Cursors.Hand;
+            btnQuestions.FlatStyle = FlatStyle.Flat;
+            btnQuestions.Font = new Font("Microsoft Sans Serif", 9.75F, FontStyle.Bold, GraphicsUnit.Point);
+            btnQuestions.Image = WinApp.Properties.Resources.quiz;
+            btnQuestions.Location = new Point(3, 329);
             btnQuestions.Name = "btnQuestions";
-            btnQuestions.Size = new Size(227, 40);
+            btnQuestions.Size = new Size(222, 40);
             btnQuestions.TabIndex = 20;
             btnQuestions.Text = "Questões";
+            btnQuestions.TextAlign = ContentAlignment.MiddleRight;
+            btnQuestions.TextImageRelation = TextImageRelation.ImageBeforeText;
             btnQuestions.UseVisualStyleBackColor = true;
             // 
             // btnTests
             // 
-            btnTests.Font = new Font("Microsoft Sans Serif", 9.75F, FontStyle.Regular, GraphicsUnit.Point);
-            btnTests.Location = new Point(1, 283);
+            btnTests.Cursor = Cursors.Hand;
+            btnTests.FlatStyle = FlatStyle.Flat;
+            btnTests.Font = new Font("Microsoft Sans Serif", 9.75F, FontStyle.Bold, GraphicsUnit.Point);
+            btnTests.Image = WinApp.Properties.Resources.receipt;
+            btnTests.Location = new Point(3, 283);
             btnTests.Name = "btnTests";
-            btnTests.Size = new Size(227, 40);
+            btnTests.Size = new Size(222, 40);
             btnTests.TabIndex = 19;
             btnTests.Text = "Testes";
+            btnTests.TextAlign = ContentAlignment.MiddleRight;
+            btnTests.TextImageRelation = TextImageRelation.ImageBeforeText;
             btnTests.UseVisualStyleBackColor = true;
             // 
             // btnDashboard
             // 
-            btnDashboard.FlatAppearance.BorderSize = 0;
-            btnDashboard.Font = new Font("Microsoft Sans Serif", 9.75F, FontStyle.Regular, GraphicsUnit.Point);
-            btnDashboard.Location = new Point(1, 167);
+            btnDashboard.Cursor = Cursors.Hand;
+            btnDashboard.FlatAppearance.BorderColor = Color.Black;
+            btnDashboard.FlatStyle = FlatStyle.Flat;
+            btnDashboard.Font = new Font("Microsoft Sans Serif", 11.25F, FontStyle.Bold, GraphicsUnit.Point);
+            btnDashboard.ForeColor = SystemColors.ControlText;
+            btnDashboard.Image = WinApp.Properties.Resources.home;
+            btnDashboard.Location = new Point(12, 166);
             btnDashboard.Name = "btnDashboard";
-            btnDashboard.Size = new Size(227, 40);
+            btnDashboard.Size = new Size(200, 54);
             btnDashboard.TabIndex = 4;
             btnDashboard.Text = "Painel de Estudos";
+            btnDashboard.TextAlign = ContentAlignment.MiddleRight;
+            btnDashboard.TextImageRelation = TextImageRelation.ImageBeforeText;
             btnDashboard.UseVisualStyleBackColor = true;
             // 
             // pictureBox1
             // 
+            pictureBox1.BackColor = Color.Transparent;
             pictureBox1.Image = WinApp.Properties.Resources.tasks_png;
             pictureBox1.Location = new Point(12, 12);
             pictureBox1.Name = "pictureBox1";
@@ -151,24 +179,24 @@
             label1.Font = new Font("Microsoft Sans Serif", 12F, FontStyle.Bold, GraphicsUnit.Point);
             label1.Location = new Point(75, 26);
             label1.Name = "label1";
-            label1.Size = new Size(137, 20);
+            label1.Size = new Size(131, 20);
             label1.TabIndex = 0;
-            label1.Text = "Mariana Testing";
+            label1.Text = "Mariana Exams";
             // 
             // panel2
             // 
             panel2.BackColor = Color.DodgerBlue;
             panel2.Dock = DockStyle.Top;
-            panel2.Location = new Point(228, 0);
+            panel2.Location = new Point(231, 0);
             panel2.Name = "panel2";
-            panel2.Size = new Size(1056, 66);
+            panel2.Size = new Size(1053, 66);
             panel2.TabIndex = 1;
             // 
             // label4
             // 
             label4.AutoSize = true;
             label4.Font = new Font("Microsoft Sans Serif", 15.75F, FontStyle.Bold, GraphicsUnit.Point);
-            label4.Location = new Point(662, 118);
+            label4.Location = new Point(660, 103);
             label4.Name = "label4";
             label4.Size = new Size(250, 25);
             label4.TabIndex = 2;
@@ -182,10 +210,11 @@
             Controls.Add(label4);
             Controls.Add(panel2);
             Controls.Add(panel1);
+            FormBorderStyle = FormBorderStyle.Fixed3D;
             Icon = (Icon)resources.GetObject("$this.Icon");
             Name = "telaPrincipal";
             StartPosition = FormStartPosition.CenterScreen;
-            Text = "MarianaTesting 1.0";
+            Text = "MarianaExams 1.0";
             panel1.ResumeLayout(false);
             panel1.PerformLayout();
             ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
